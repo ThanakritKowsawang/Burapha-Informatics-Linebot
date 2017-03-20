e36af63fc0e5fac50fe480e151812eef7e65188a
¹ÒÂ¸¹¡Äµ â¤éÇÊÇèÒ§ <56160329@angsila.buu.ac.th>
2017-03-20 20:50:28 +0700

    add connect db

---
+++ db.php
@@ -0,0 +1,37 @@
+<?php
+
+class db {
+	private $host="localhost";
+	private $username="it56160329";
+	private $password="81Wuuzm1";
+	private $dbname="it56160329";
+	public 	$link;
+	public 	$results;
+
+	function connect(){
+		if ($this->link = new PDO("mysql:host=$this->host; dbname=$this->dbname",
+									$this->username,$this->password)){
+			$this->link->exec("SET CHARACTER SET utf8");
+			return true;
+		}else {
+			// echo "Could not linkect to the database!!";
+			return false;
+		}
+	}
+
+	function query($sql){
+		if ($this->results=$this->link->query($sql)) {
+			return ($this->results);
+		}else{
+			// echo "Could not query data form database";
+			return false;
+		}
+	}
+
+	function close(){
+		$this->link = null;
+	}
+
+}
+
+?>
